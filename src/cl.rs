244ad93a2080d51a44b9f4cd5a5656b952dfbc04
yoh <jorgentau@gmail.com>
2022-03-28 23:21:45 +0300

    better error handling

--- src/cl.rs
+++ src/cl.rs
@@ -1,17 +1,19 @@
 use std::env;
+use std::num::IntErrorKind;
 
-const DEF_PR: usize = 2;
-const MAX_PR: usize = 8;
-const MAX_BYTES: u64 = 18446744073709499999;
+const DEF_P: usize = 2;
+const MAX_P: usize = 8;
+const MAX_BYTES: u128 = 1024u128.pow(8);
 const ARG_B: &str = "--bytes=";
 const ARG_P: &str = "--precision=";
 const ARG_B_SH: &str = "-b";
 const ARG_P_SH: &str = "-p";
 
-pub fn parse_args() -> (u64, usize) {
+pub fn parse_args() -> (u128, usize, String) {
   let args: Vec<String> = env::args().collect();
-  let mut bytes: u64 = 0;
+  let mut bytes: u128 = 0;
   let mut precision: usize = 2;
+  let mut huge_indicator: String = "".to_owned();
 
   for idx in 0..args.len() {
     let arg = &args[idx];
@@ -19,14 +21,20 @@ pub fn parse_args() -> (u64, usize) {
     if arg == ARG_B_SH || arg.starts_with(ARG_B) {
       let intermediate = match arg == ARG_B_SH {
         true => args[idx + 1].to_owned(),
-        false => arg.strip_prefix(ARG_B).unwrap().to_string(),
+        false => arg.strip_prefix(ARG_B).unwrap().to_owned(),
       };
 
-      bytes = intermediate
-        .parse::<u64>()
-        .expect("Invalid argument")
-        .clamp(0, MAX_BYTES);
-
+      bytes = match intermediate.parse::<u128>() {
+        Ok(value) => value,
+        Err(err) => match err.kind() {
+          IntErrorKind::InvalidDigit => panic!("Bytes must be a number"),
+          IntErrorKind::PosOverflow => {
+            huge_indicator = "+".to_owned();
+            MAX_BYTES
+          }
+          _ => panic!("Invalid bytes"),
+        }
+      }
     } else if arg == ARG_P_SH || arg.starts_with(ARG_P) {
       let intermediate = match arg == ARG_P_SH {
         true => args[idx + 1].to_owned(),
@@ -35,11 +43,11 @@ pub fn parse_args() -> (u64, usize) {
 
       precision = intermediate
         .parse::<usize>()
-        .unwrap_or(DEF_PR)
-        .clamp(0, MAX_PR);
+        .unwrap_or(DEF_P)
+        .clamp(0, MAX_P);
 
     }
   }
 
-  (bytes, precision)
+  (bytes, precision, huge_indicator)
 }
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@
 mod cl;
 
 const KIBI: u128 = 1024;
-const SIZES: &[&str] = &["Bytes", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB"];
+const SIZES: &[&str] = &["Bytes", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
 
 
 fn main() {
-    let (bytes, precision) = cl::parse_args();
+    let (bytes, precision, huge_indicator) = cl::parse_args();
 
     let power: u32 = match bytes == 0 {
         true => 0,
@@ -16,5 +16,5 @@ fn main() {
     let res: f64 = bytes as f64 / KIBI.pow(power) as f64;
     let size = SIZES[power as usize];
 
-    println!("{:.*} {}", precision, res, size);
+    println!("{}{:.*} {}", huge_indicator, precision, res, size);
 }
